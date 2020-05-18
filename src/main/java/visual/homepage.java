ff899c95f251911cf48d2d4793b35de173ef2145
Вовочка <boosth14@gmail.com>
2020-05-18 22:49:03 +0300

    main

--- src/main/java/visual/homepage.java
+++ src/main/java/visual/homepage.java
@@ -1,7 +1,34 @@
 package visual;
 
+import java.util.Scanner;
+
 public class homepage {
     public static void main(String[] args) {
+        homepage run = new homepage();
+        homepage.run();
 
+    }
+    public static void run(){
+        int inputValue = 0;
+        System.out.println("+---------------------------+");
+        System.out.println("|1. Знайти товар            |");
+        System.out.println("|2. Знайти товар            |");
+        System.out.println("|1. Знайти товар            |");
+        System.out.println("|1. Знайти товар            |");
+        System.out.println("|1. Знайти товар            |");
+        System.out.println("|1. Знайти товар            |");
+        System.out.println("|1. Знайти товар            |");
+        System.out.println("+---------------------------+");
+        Scanner scan = new Scanner(System.in);
+        inputValue = scan.nextInt();
+        switch (inputValue){
+            case 1:
+                System.out.println("entered " + inputValue);
+                break;
+            default:
+                System.out.println("Помилка.\nВи ввели хибне число - " + inputValue+", введіть число від 1 до 12");
+                run();
+
+        }
     }
 }
