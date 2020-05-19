3c29ba1a091377a4300ed5c289b2575aa85d16e0
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-05-19 16:08:36 +0300

    fix: refactor project structure.

--- src/main/java/views/soap/Input.java
+++ src/main/java/views/soap/Input.java
@@ -9,9 +9,9 @@ public class Input{
     SoapDao soapDAO = new SoapImplDao();
 
     public Input() throws Exception {
-        System.out.println("Введіть назву мила");
-        String name = new Scanner(System.in).nextLine();
-        System.out.println("Введіть ціну");
-        String price = new Scanner(System.in).next();
+//        System.out.println("Введіть назву мила");
+//        String name = new Scanner(System.in).nextLine();
+//        System.out.println("Введіть ціну");
+//        String price = new Scanner(System.in).next();
     }
 }
